7193050a2f8b1672329721017553eda72760b373
AlperenKizlirmak <118993006+AlperenKizlirmak@users.noreply.github.com>
2023-07-16 21:06:16 +0300

    Add files via upload

---
+++ dom.js
@@ -0,0 +1,22 @@
+const button =document.getElementById("btn")
+button.style.backgroundColor = "black"
+button.style.color =  "yellow"
+button.style.padding = "0.7rem"
+button.style.border = "none"
+button.style.borderRadius = "10px"
+button.style.fontSize = "20px" 
+textInput.style.padding = "0.7rem"
+
+
+//*====================
+const list = document.getElementByTagName("li")
+console.log(list)
+
+
+
+
+
+const header= documnet.getElementByTagName("header")
+console.log(header[0])
+
+console.log(list.item(3))
---
+++ oop5.js
@@ -0,0 +1,63 @@
+//* =============================================
+//*        OOP - Object Constructor (ES5)
+//* =============================================
+
+//* Javascript, prototype-temelli bir dildir.
+//* Butun JavaScript nesneleri (objects) ozellikleri (properties)
+//* ve metotlari bir prototipten miras olarak almaktadir.
+//* Object prototipi, prototip miras zincirinin en tepesinde yer almaktadir.
+//* Ornegin Date ve Array nesneleri Object.prototype'dan miras almaktadir.
+
+//? Object Constructor
+
+function Book(title, author, year) {
+  this.title = title
+  this.author = author
+  this.year = year
+  //   this.getSummary = function () {
+  //     return `${this.title} was written by ${this.author} in ${this.year}`
+  //   }
+}
+
+//* Ornegin Book nesnesinin tum instance'lari getSummary() fonksiyonunu miras alabilir.
+//* Ancak, getSummary() fonksiyonu bellekte sadece bir yer kaplamaktadir.
+
+Book.prototype.getSummary = function () {
+  return `${this.title} was written by ${this.author} in ${this.year}`
+}
+
+//! Book kalibinda yeni bir ornek (instance) olusturduk.
+const book1 = new Book("Kasagi", "Ömer Seyfettin", 1920)
+console.log(book1)
+console.log(book1.getSummary())
+
+//* Bir nesnenin prototiplerine .prototype ile erisilebilir.
+//* Ancak bir instance'in prototiplerine .__proto__ ile erisilmektedir.
+console.log(Book.prototype)
+console.log(book1.__proto__)
+
+//? instance
+const book2 = new Book("Simyaci", "Pauolo Coelho", 1990)
+console.log(book2.getSummary())
+
+// console.log(new Date().getFullYear())
+
+
+//? IHERİTANCE  
+
+//? Sub-Class
+
+function Magazine(title,author,year,month){
+ //?INHERİTANCE
+Book.call(this, title, author, year)
+this.month = month
+}
+
+//! Prototipleri miras almak için Object
+
+Magazine.prototype = Object.create(Book.prototype)
+const mag1 = new Magazine("SRE", "Einstion", 1930, "Nov")
+console.log(mag1)
+
+//!Protoiplerdoğrudan miras olarak gelmez
+console.log(mag1.getSummary())
